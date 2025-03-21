6e888e528eca953382b729edbadac0b1b19a6df8
Biplab Khanal <147678085+Biplabkhanal@users.noreply.github.com>
2025-03-21 15:29:22 +0545

    redirect issue fixed

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = [
+        'sender_id',
+        'recipient_id',
+        'message',
+        'attachment',
+        'is_read'
+    ];
+
+    protected $casts = [
+        'is_read' => 'boolean'
+    ];
 }
